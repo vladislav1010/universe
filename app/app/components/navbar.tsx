8e771e7ac33b523435d115bad1741d5a9674fae0
Владислав Савченко <vs101ff@gmail.com>
2021-12-14 11:56:31 +0000

    feat: add nav items object

--- app/app/components/navbar.tsx
+++ app/app/components/navbar.tsx
@@ -5,9 +5,36 @@ import { json, LoaderFunction } from "remix";
 import { i18n } from "~/i18n.server"; // this is the first file you created
 import { useTranslation } from "react-i18next";
 
+type NavItem = {
+  name: string;
+} & (
+  | { children: NavItem[]; to?: never }
+  | {
+      to: string;
+      children?: never;
+    }
+);
+
 export default function Navbar() {
   let { t } = useTranslation("common");
-  const LINKS = [{ name: t("navbar.links.services"), to: "/services" }];
+  const LINKS: NavItem[] = [
+    { name: t("navbar.links.services"), to: "/services" },
+    {
+      name: t("navbar.links.test.name"),
+      children: [
+        { name: t("navbar.links.test.children.test1"), to: "/test/test1" },
+        {
+          name: t("navbar.links.test.children.test1.children.test2.name"),
+          children: [
+            {
+              name: t("navbar.links.test.children.test1.children.test2.test3"),
+              to: "/test/test2/test3",
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-  return <h1>{LINKS[0].name}</h1>;
+  return <h1>{JSON.stringify(LINKS)}</h1>;
 }
