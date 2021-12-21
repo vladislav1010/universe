7fbeacb37ac78a4d34740b3265720c1d05f4a784
Владислав Савченко <vs101ff@gmail.com>
2021-12-21 06:54:54 +0000

    feat: integrate FeedbackForm into index route, temporrary set color team

--- app/app/routes/index.tsx
+++ app/app/routes/index.tsx
@@ -2,6 +2,7 @@ import type {MetaFunction, LoaderFunction} from 'remix'
 import {json} from 'remix'
 import {i18n} from '../i18n.server'
 import * as React from 'react'
+import { FeedbackForm } from '../components/feedback-form'
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
@@ -10,7 +11,7 @@ import * as React from 'react'
 export const loader: LoaderFunction = async ({request}) => {
   // https://remix.run/api/remix#json
   return json({
-    i18n: await i18n.getTranslations(request, ['common']),
+    i18n: await i18n.getTranslations(request, ['common', 'feedback']),
   })
 }
 
@@ -24,5 +25,8 @@ export const meta: MetaFunction = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  return <main />
+  // TODO: remove color team className
+  return <main className='set-color-team-current-unknown'>
+    <FeedbackForm />
+  </main>
 }
