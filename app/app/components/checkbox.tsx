1e9f1e6c6b326fe7655e80b72c3897098cb57d79
Владислав Савченко <vs101ff@gmail.com>
2022-01-08 12:28:04 -0500

    fix: simplify and fix useToggle

--- app/app/components/checkbox.tsx
+++ app/app/components/checkbox.tsx
@@ -1,35 +1,32 @@
-import { usePrevious } from '@chakra-ui/hooks';
 import * as React from 'react'
-import {useControlledSwitchWarning, useOnChangeReadOnlyWarning} from '~/util/react/controlled';
+import {
+  useControlledSwitchWarning,
+  useOnChangeReadOnlyWarning,
+} from '~/util/react/controlled'
 
-function deferred<T, TRejectReason>() {
-  let resolve: (value: T) => void, reject: (reason: TRejectReason) => void;
-  const promise = new Promise<T>((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  return {promise, resolve, reject}
-}
-
-function callAll<T extends unknown[]>(...fns: (((...args: T) => void) | undefined)[]) { 
+function callAll<T extends unknown[]>(
+  ...fns: (((...args: T) => void) | undefined)[]
+) {
   return (...args: T) => fns.forEach(fn => fn?.(...args))
 }
 
 interface UseToggleProps {
-  initialIsActive?: boolean;
-  onChange?: (isActive: boolean) => void;
-  isActive?: boolean;
-  readOnly?: boolean;
+  initialIsActive?: boolean
+  onChange?: (isActive: boolean) => void
+  isActive?: boolean
+  readOnly?: boolean
 }
 
 // https://reactjs.org/blog/2020/08/10/react-v17-rc.html#other-breaking-changes
 // Additionally, React 17 will always execute all effect cleanup functions (for all components) before it runs any new effects.
-const useToggle = ({initialIsActive = false, onChange, isActive: controlledIsActive,readOnly = false}: UseToggleProps) => {
+const useToggle = ({
+  initialIsActive = false,
+  onChange,
+  isActive: controlledIsActive,
+  readOnly = false,
+}: UseToggleProps) => {
   const {current: initialState} = React.useRef(initialIsActive)
-  const [state, setState] = React.useState<boolean>(initialState);
+  const [state, setState] = React.useState<boolean>(initialState)
 
   if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -50,42 +47,30 @@ const useToggle = ({initialIsActive = false, onChange, isActive: controlledIsAct
   const isActiveIsControlled = controlledIsActive != null
   const isActive = isActiveIsControlled ? controlledIsActive : state
 
-  const previousState = usePrevious(state);
-  const stateDeferredRef = React.useRef<{promise: Promise<boolean>; resolve: (value: boolean) => void; reject: (reason: never) => void;} | null>(null)
-  React.useEffect(() => {
-    // if stateDeferredRef.current is null, client code simply didn't called dispatchWithOnChange
-
-    if (!isActiveIsControlled && state !== previousState && stateDeferredRef.current) {
-      stateDeferredRef.current.resolve(state); 
-    }
-  }, [isActiveIsControlled, previousState, state])
-
-  async function dispatchWithOnChange(action: React.SetStateAction<boolean>) {    
+  async function dispatchWithOnChange(action: React.SetStateAction<boolean>) {
     if (!isActiveIsControlled) {
-      if (action instanceof Function) {
-      // https://reactjs.org/docs/hooks-faq.html#is-there-something-like-instance-variables
-      // typically you want to modify refs in event handlers and effects
-      stateDeferredRef.current = deferred<boolean, never>()
-      }
       // https://github.com/kentcdodds/react-hooks/blob/main/src/exercise/06.md#3--store-the-state-in-an-object
       // the function probably is called asynchronously by client code
       setState(action)
     }
 
-    if (!(action instanceof Function)) {
-      onChange?.(action)
-    } else if (isActiveIsControlled) {
+    if (action instanceof Function) {
       onChange?.(action(isActive))
     } else {
-      onChange?.(await stateDeferredRef.current!.promise)
+      onChange?.(action)
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function getTogglerProps({onClick, ...props}: React.HTMLAttributes<any> = {}) {
+  function getTogglerProps({
+    onClick,
+    ...props
+  }: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  React.HTMLAttributes<any> = {}) {
     return {
       'aria-pressed': isActive,
-      onClick: callAll(onClick, async () => dispatchWithOnChange((_isActive) => !_isActive)),
+      onClick: callAll(onClick, async () =>
+        dispatchWithOnChange(_isActive => !_isActive),
+      ),
       ...props,
     }
   }
@@ -97,8 +82,23 @@ const useToggle = ({initialIsActive = false, onChange, isActive: controlledIsAct
   }
 }
 
-const Checkbox = ({isActive: controlledIsActive, onChange, initialIsActive, readOnly, ...inputProps}: Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type' | 'readOnly' | 'onChange' | 'value'> & UseToggleProps) => {
-  const {getTogglerProps, isActive} = useToggle({isActive: controlledIsActive, onChange, initialIsActive, readOnly})
+const Checkbox = ({
+  isActive: controlledIsActive,
+  onChange,
+  initialIsActive,
+  readOnly,
+  ...inputProps
+}: Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'type' | 'readOnly' | 'onChange' | 'value'
+> &
+  UseToggleProps) => {
+  const {getTogglerProps, isActive} = useToggle({
+    isActive: controlledIsActive,
+    onChange,
+    initialIsActive,
+    readOnly,
+  })
 
   return (
     <div className="">
@@ -109,4 +109,4 @@ const Checkbox = ({isActive: controlledIsActive, onChange, initialIsActive, read
   )
 }
 
-export default Checkbox;
+export default Checkbox
