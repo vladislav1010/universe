f0c12af08b13e014864760e6a64ce3294430d7be
Владислав Савченко <vs101ff@gmail.com>
2022-01-08 22:49:05 -0500

    fix: form label default color

--- app/app/components/form-control/styles.ts
+++ app/app/components/form-control/styles.ts
@@ -1,10 +1,17 @@
-import { StylesProviderContext } from "./form-control"
+import {StylesProviderContext} from './form-control'
 
 const styles: StylesProviderContext = {
-  form: {container: 'w-full relative', helperText: 'mt-2 text-secondary leading-normal text-sm'},
+  form: {
+    container: 'w-full relative',
+    helperText: 'mt-2 text-secondary leading-normal text-sm',
+  },
   requiredIndicator: 'ml-1 text-red-500',
-  label: 'text-base mr-3 mb-2 font-medium transition-opacity duration-200 opacity-100 disabled:opacity-40',
-  formError: {icon: 'mr-[0.5em] text-red-500', text: 'text-red-500 mt-2 text-sm'}
+  label:
+    'text-base mr-3 mb-2 font-medium transition-opacity duration-200 opacity-100 disabled:opacity-40 text-gray-400',
+  formError: {
+    icon: 'mr-[0.5em] text-red-500',
+    text: 'text-red-500 mt-2 text-sm',
+  },
 }
 
 export {styles}
